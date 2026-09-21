3962b9d208faea1c28589ac6d941d895aa22d56d
notastrocat <archit42.sharma@gmail.com>
2026-09-22 02:18:13 +0530

    fix: change the `run.sh` script
    to fix crashes in CI test-runs.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/build/install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/build/install/lib
 
 ./build/install/bin/tiny-c-projects "John Doe"
 echo -e "\n---"
